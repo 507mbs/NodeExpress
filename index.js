6f4cf0b39fa1ac35e87170fe9c16a42458de71cb
507mbs <507mbs@gmail.com>
2023-05-15 22:31:15 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import express from 'express'
+// import require from 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const app=express()
+const port=3000;
+
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 sdk.auth('rnd_uUxYad...');
