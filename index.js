848026cb83734e623b93a1c0cf2fb93e18bba26f
507mbs <507mbs@gmail.com>
2023-05-16 08:19:51 +0300

    newkeyapi

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const port=3000;
 
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
-sdk.auth('rnd_uUxYad...');
+sdk.auth('rnd_p4clXcS2MQIC6eseH0dxldCmwKxZ');
 sdk.getServices({limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
-
   app.get('/',(req,res)=>{
     res.send('hello world')
 })
